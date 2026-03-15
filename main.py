dd613dae3d005668910184b977a3cb2eb5ea65ed
shikiso <lewisemerson00@gmail.com>
2026-03-15 23:47:08 +0800

    yurrrr

--- main.py
+++ main.py
@@ -12,6 +12,53 @@ intents = discord.Intents.default()
 intents.message_content = True  # Enable the message content intent
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+class Player:
+    def __init__(self):
+        self.cards = []
+        self.count = 0
+
+    def deal_card(self, card): # [1] = num, [0] = suit
+        self.cards.append(card)
+        if card[1] > 10: # if card is a picture card set the value back to 10
+            self.count += 10
+        else:
+            self.count += card[1]
+    
+    def check_cards(self):
+        # Check for blackjack
+        if self.cards[0][1] > 10 and self.cards[1][1] == 1 or self.cards[1][1] > 10 and self.cards[0][1] == 1:
+            return 1
+        
+        # Check if player went over 21
+        if self.count > 21:
+            return 2
+
+        return 0
+
+    def get_cards_string(self):
+        text = ""
+        temp = ""
+
+        for card in self.cards:
+            if card[1] == 1:
+                temp = f"A{card[0]} "
+            elif card[1] == 11:
+                temp = f"J{card[0]} "
+            elif card[1] == 12:
+                temp = f"Q{card[0]} "
+            elif card[1] == 13:
+                temp = f"K{card[0]} "
+            else:
+                temp = f"{str(card[1])}{card[0]} "
+            text += temp
+        return text
+
+async def ban(member):
+        try:
+            await member.ban(reason="BlackJack")
+        except Exception as e:
+            print("Error!", e)
+
 # Event handler for when the bot is ready
 @bot.event
 async def on_ready():
@@ -49,10 +96,14 @@ async def slots(ctx, member:discord.Member):
             print("Error!", e)
 
 @bot.command()
-async def blackjack(ctx, member:discord.Member):
-    dealers_count = 0
-    players_count = 0
-    cards = [0,1,2,3,4,5,6,7,8,9,10,11,12,13] # 0 = Ace, 11 = Jack, 12 = Queen, 13 = King
+async def blackjack(ctx, member:discord.Member):  
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
+
+    player = Player()
+    dealer = Player()
+
+    cards = [1,2,3,4,5,6,7,8,9,10,11,12,13] # 0 = Ace, 11 = Jack, 12 = Queen, 13 = King
     diamond = ":large_blue_diamond:"
     heart = ":heart:"
     spade = ":spades:"
@@ -62,41 +113,54 @@ async def blackjack(ctx, member:discord.Member):
     await ctx.send(f"{member.mention} has been chosen for BlackJack!\nBeat the dealer to win! :partying_face:")
     
     # inital dealing
-    players_count += choice(cards)
-    dealers_count += choice(cards)
-    players_count += choice(cards)
-    
-    while 1:
-        await ctx.send(f"Player has {str(players_count)}")
-        await ctx.send(f"Dealer has {str(dealers_count)}")
+    player.deal_card((choice(suits), choice(cards)))
+    dealer.deal_card((choice(suits), choice(cards)))
+    player.deal_card((choice(suits), choice(cards)))
 
+    if player.check_cards() == 1:
+        await ctx.send("Player has BlackJack!")
+        dealer.deal_card((choice(suits), choice(cards)))
+        if dealer.check_cards() == 1:
+            await ctx.send("Dealer also has BlackJack!\nNo one wins ;(")
+        else:
+            await ctx.send("Player wins!")
+     
+    # Game loop
+    while 1:
+        await ctx.send(f"Dealer has {str(dealer.count)} ({dealer.get_cards_string()})")
+        await ctx.send(f"Player has {str(player.count)} ({player.get_cards_string()})")
+        if player.count == 21:
+            break
         await ctx.send(f"{member.mention} would you like to hit or stay?")
+
         try:
-            message = await bot.wait_for("message", timeout=60.0)
-            if message.content == "hit":
-                players_count += choice(cards)
-                if players_count > 21:
-                    await ctx.send(f"Player has {str(players_count)}")
-                    await ctx.send("Player has bust!")
+            message = await bot.wait_for("message", check=check, timeout=60.0)
+            if message.content.lower() == "hit":
+                player.deal_card((choice(suits), choice(cards)))
+                if player.check_cards() == 2:
+                    await ctx.send("Player has bust! Later nerd...")
+                    await ban(member)
                     break
-            elif message.content == "stay":
+
+            elif message.content.lower() == "stay":
                 break
+
         except asyncio.TimeoutError:
             await ctx.send(f"{member.mention} has failed to reply!")
-            await member.ban(reason="Blackjack timeout")
+            await ban(member)
     
-    if players_count < 22:
-        while dealers_count < 17:
-            dealers_count += choice(cards)
-            await ctx.send(f"Dealer has {str(dealers_count)}")
-        if dealers_count > 21:
-            await ctx.send("Dealer busts Player wins!")
-        elif dealers_count > players_count:
-            await ctx.send("Dealer wins!")
-        elif dealers_count < players_count:
-            await ctx.send("Player wins!")
+    if player.count < 22:
+        while dealer.count < 17:
+            dealer.deal_card((choice(suits), choice(cards)))
+
+        await ctx.send(f"Dealer has {str(dealer.count)} ({dealer.get_cards_string()})")
+        if dealer.check_cards() == 2:
+            await ctx.send("Dealer has bust! Player wins!")
+        elif dealer.count > player.count:
+            await ctx.send("Dealer beats player! Loser get banned")
+            await ban(member)
         else:
-            await ctx.send("Draw")
+            await ctx.send("Player has won!")
 
 
 # Run the bot with your token
